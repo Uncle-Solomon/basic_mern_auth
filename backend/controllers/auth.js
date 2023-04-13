b65893ef1b8c81649065fdac4c43aeafeaf5e910
Ameh Solomon Onyeke <amehsolomon46@gmail.com>
2023-04-13 17:00:51 +0100

    Working on controllers

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -45,18 +45,45 @@ const signup = (req, res, next) => {
 
 const login = (req, res) => {
   let { email, password } = req.body;
-  User.findOne({ email: email }).then((user) => {
-    if (!user) {
-      return res.status(404).json({ errors: [{ user: "not found" }] });
-    } else
-      [
-        bcrypt.compare(password, user.password).then((isMatch) => {
-          if (!isMatch) {
-            return res
-              .status(400)
-              .json({ errors: [{ password: "incorrect" }] });
-          }
-        }),
-      ];
-  });
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({ errors: [{ user: "not found" }] });
+      } else {
+        bcrypt
+          .compare(password, user.password)
+          .then((isMatch) => {
+            if (!isMatch) {
+              return res
+                .status(400)
+                .json({ errors: [{ password: "incorrect" }] });
+            }
+            let access_token = createJWT(user.email, user._id, 3600);
+            jwt.verify(
+              access_token,
+              process.env.TOKEN_SECRET,
+              (err, decoded) => {
+                if (err) {
+                  res.status(500).json({ error: err });
+                }
+                if (decoded) {
+                  return res
+                    .status(500)
+                    .json({
+                      success: true,
+                      token: access_token,
+                      message: user,
+                    });
+                }
+              }
+            );
+          })
+          .catch((err) => {
+            res.status(500).json({ errors: err });
+          });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ errors: err });
+    });
 };
